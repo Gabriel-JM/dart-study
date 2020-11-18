c16ea3257fac94b9d3f218b442011c2a949fc297
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-11-17 23:12:28 -0300

    Generate a console full project with stagehand

---
+++ stagehand-playground/bin/stagehand_playground.dart
@@ -0,0 +1,5 @@
+import 'package:stagehand_playground/stagehand_playground.dart' as stagehand_playground;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stagehand_playground.calculate()}!');
+}
---
+++ stagehand-playground/test/stagehand_playground_test.dart
@@ -0,0 +1,8 @@
+import 'package:stagehand_playground/stagehand_playground.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
